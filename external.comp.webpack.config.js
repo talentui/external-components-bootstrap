68c9f53f8aa8213b2c24e9a63b4da24982fb60d4
lihe3 <lihe3@corp.beisencorp.com>
2018-02-07 17:27:14 +0800

    build:完善打包入口文件设置

--- external.comp.webpack.config.js
+++ external.comp.webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const appRoot = process.cwd();
 const { dllList } = require(path.resolve(appRoot, "./src/dll-config.js"));
 module.exports = require("@talentui/umd-module-webpack-config")({
-    entry: ["babel-polyfill", "@talentui/external-components-bootstrap/src/index.js"],
+    entry: ["babel-polyfill", "@talentui/external-components-bootstrap"],
     dllList: dllList,
     alias: {
         "&": path.resolve(appRoot, "./src")
