974d2848b331b801ee8cc6f42bf64e76e37e9d49
lihe3 <lihe3@corp.beisencorp.com>
2018-02-09 11:10:58 +0800

    remove useless file

--- src/index.js
+++ src/index.js
@@ -9,9 +9,7 @@ import componentRegistry from "@talentui/external-component-registry";
 /**
  * webpack 按需加载，设置public path
 */
-__webpack_public_path__ = `//stnew03.beisen.com/ux/upaas/${
-    process.env.packageName
-}/release/dist/`;
+__webpack_public_path__ = "//stnew03.beisen.com/ux/upaas/" + process.env.packageName + "/release/dist/";
 /**
  * 组件注册
 */
