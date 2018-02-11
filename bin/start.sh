e6dee04df7f8228f3bb0d769fff6f44133cf3780
lihe3 <lihe3@corp.beisencorp.com>
2018-02-11 14:51:07 +0800

    build:add bin

---
+++ bin/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+friendly=on  dev_server=on \
+asset_path=http://stnew03.beisen.com/ux/upaas-portal/release/dist/  \
+webpack-dev-server --config ./node_modules/@talentui/external-components-bootstrap/webpack.config.js
